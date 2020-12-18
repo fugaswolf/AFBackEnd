0b6188c3ef657838068c097ec031632c3c718d81
fu6as <alvi.istamalov@gmail.com>
2020-12-18 15:45:19 +0100

    small bug fix: show products by category Id

--- src/main/java/ehb/alvi/afbackendapi/dao/ProductRepository.java
+++ src/main/java/ehb/alvi/afbackendapi/dao/ProductRepository.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    Page<Product> findByCategoryId(@RequestParam("id") Long Id, Pageable pageable);
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 
 }
 
