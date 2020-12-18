2a19030170ca97d76a920e6e5ea77660f2994973
fu6as <alvi.istamalov@gmail.com>
2020-12-18 17:32:08 +0100

    expose id of productcategory entities

--- src/main/java/ehb/alvi/afbackendapi/config/DataRestConfig.java
+++ src/main/java/ehb/alvi/afbackendapi/config/DataRestConfig.java
@@ -2,15 +2,28 @@ package ehb.alvi.afbackendapi.config;
 
 import ehb.alvi.afbackendapi.entity.Product;
 import ehb.alvi.afbackendapi.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 
 //Zorgt ervoor dat Spring dit ziet als een configuratie
 @Configuration
 public class DataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public DataRestConfig(EntityManager theEntityManager){
+        entityManager = theEntityManager;
+    }
 
     //Dit stukje code zorgt ervoor dat de PUT, DELETE & POST http methods niet bereikbaar zijn
     //Hiermee overriden we de default REST API configuratie
@@ -29,6 +42,30 @@ public class DataRestConfig implements RepositoryRestConfigurer {
                 .forDomainType(ProductCategory.class)
                 .withItemExposure((metdata,httpMethods) -> httpMethods.disable(unSupportedActions))
                 .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unSupportedActions));
+        // internal helper method call to expose the ids
+        exposeIds(config);
+    }
+
+    public void exposeIds(RepositoryRestConfiguration config) {
+
+        // entity ids exposen
+
+
+        // get list of all entity classes from the entity manager
+        Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
+
+        // create an array of the entity types
+        List<Class> entityClasses = new ArrayList<>();
+
+        // get the entity types for the entities
+        for(EntityType tempEntityType: entities) {
+            entityClasses.add(tempEntityType.getJavaType());
+        }
+
+        // entity ids exposen voor de array van entity/domain types
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
+
 
     }
 }
